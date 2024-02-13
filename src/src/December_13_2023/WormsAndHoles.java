8093dce21fbe1d5faad68b955ef32c16a4cbea89
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-02-14 00:09:47 +0200

    Update WormsAndHoles.java

--- src/src/December_13_2023/WormsAndHoles.java
+++ src/src/December_13_2023/WormsAndHoles.java
@@ -5,67 +5,80 @@ import java.util.*;
 public class WormsAndHoles {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int[] wormsInput = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int[] holesInput = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
         Deque<Integer> wormsStack = new ArrayDeque<>();
-        Deque<Integer> holesQueue = new ArrayDeque<>();
+        Deque<Integer> holessQueue = new ArrayDeque<>();
+
+        for (int i : wormsInput) {
+            wormsStack.push(i);
+        }
+        for (int i : holesInput) {
+            holessQueue.offer(i);
+        }
+
         int matches = 0;
 
-        Arrays.stream(scanner.nextLine().split(" "))
-                .map(Integer::parseInt).
-                forEach(wormsStack::push);
-
-        Arrays.stream(scanner.nextLine().split(" "))
-                .map(Integer::parseInt).
-                forEach(holesQueue::offer);
-        while (!wormsStack.isEmpty() && !holesQueue.isEmpty()) {
-            int currentWorm = wormsStack.pop();
-            int currentHole = holesQueue.poll();
+        while (!wormsStack.isEmpty() && !holessQueue.isEmpty()) {
+            int currentWorm = wormsStack.peek();
+            int currentHole = holessQueue.peek();
             if (currentWorm == currentHole) {
+                wormsStack.pop();
+                holessQueue.poll();
                 matches++;
-            } else if (currentWorm > currentHole) {
+            } else {
+                holessQueue.poll();
                 currentWorm -= 3;
                 if (currentWorm > 0) {
+                    wormsStack.pop();
                     wormsStack.push(currentWorm);
+                } else {
+                    wormsStack.pop();
                 }
             }
-
         }
-        if (matches > 0) {
-            System.out.println("Matches: " + matches);
+        if (matches == 0) {
+            System.out.printf("There are no matches.%n");
         } else {
-            System.out.println("There are no matches.");
+            System.out.printf("Matches: %d%n", matches);
         }
-        if(matches == wormsStack.size()){
-            System.out.println("Every worm found a suitable hole!");
+        if (wormsStack.isEmpty()) {
+            if (matches == wormsInput.length) {
+                System.out.printf("Every worm found a suitable hole!%n");
+            } else {
+                System.out.printf("Worms left: none%n");
+            }
+        } else {
+            StringBuilder stringBuilder = new StringBuilder();
+            List<Integer> leftWorms = new ArrayList<>();
+            while (!wormsStack.isEmpty()) {
+                leftWorms.add(0, wormsStack.pop());
+            }
+            for (int i = 0; i < leftWorms.size() - 1; i++) {
+                stringBuilder.append(leftWorms.get(i));
+                stringBuilder.append(", ");
+            }
+            stringBuilder.append(leftWorms.get(leftWorms.size() - 1));
+            System.out.printf("Worms left: %s%n", stringBuilder);
         }
-        if(wormsStack.isEmpty() && matches!=wormsStack.size()){
-            System.out.println("Worms left: none");
-        }
-
-        if(!wormsStack.isEmpty()){
-            System.out.print("Worms left: ");
-            while (!wormsStack.isEmpty()){
-            System.out.print(wormsStack.pollLast());
-                if(!wormsStack.isEmpty()){
-                    System.out.print(", ");
-                }else {
-                    System.out.println();
+        if (holessQueue.isEmpty()) {
+            System.out.printf("Holes left: none%n");
+        } else {
+            StringBuilder stringBuilder = new StringBuilder();
+            while (!holessQueue.isEmpty()) {
+                stringBuilder.append(holessQueue.poll());
+                if (!holessQueue.isEmpty()) {
+                    stringBuilder.append(", ");
                 }
             }
+            System.out.printf("Holes left: %s%n", stringBuilder);
+            
         }
-
-        if(holesQueue.isEmpty()){
-            System.out.println("Holes left: none");
-        }else {
-            System.out.print("Holes left: ");
-            while (!holesQueue.isEmpty()){
-                System.out.print(holesQueue.poll());
-                if(!holesQueue.isEmpty()){
-                    System.out.print(", ");
-                }else {
-                    System.out.println();
-                }
-            }
-        }
-
     }
 }
