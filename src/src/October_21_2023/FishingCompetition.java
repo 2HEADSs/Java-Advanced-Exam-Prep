e7ffd14994898dd6be88a84a7b2c1629cfba445d
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-02-15 23:05:11 +0200

    Update FishingCompetition.java

--- src/src/October_21_2023/FishingCompetition.java
+++ src/src/October_21_2023/FishingCompetition.java
@@ -1,116 +1,101 @@
 package October_21_2023;
 
-import java.util.Map;
 import java.util.Scanner;
 
+import static java.lang.Character.isDigit;
+
 public class FishingCompetition {
-    public static int rolVessel;
+    public static int rowVessel;
     public static int colVessel;
-    public static boolean printMatrix = true;
-    public static int fishAmount = 0;
+    public static boolean fallIntoWhirlpool = false;
+    public static int caughtFish = 0;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int dimensions = Integer.parseInt(scanner.nextLine());
-        char[][] matrix = new char[dimensions][dimensions];
-        for (int r = 0; r < dimensions; r++) {
+
+        int dimension = Integer.parseInt(scanner.nextLine());
+        char[][] matrix = new char[dimension][dimension];
+
+        for (int r = 0; r < dimension; r++) {
             String row = scanner.nextLine();
             matrix[r] = row.toCharArray();
             int index = row.indexOf('S');
             if (index != -1) {
-                rolVessel = r;
+                rowVessel = r;
                 colVessel = index;
             }
         }
-
-        boolean stilMoving = true;
-        String direction = scanner.nextLine();
-        while (stilMoving && !direction.equals("collect the nets")) {
-            if (direction.equals("up")) {
-                if (isInBound(matrix, rolVessel - 1, colVessel)) {
-                    stilMoving = move(matrix, rolVessel - 1, colVessel);
-                } else {
-                    stilMoving = move(matrix, matrix.length - 1, colVessel);
-                }
-            } else if (direction.equals("down")) {
-                if (isInBound(matrix, rolVessel + 1, colVessel)) {
-                    stilMoving = move(matrix, rolVessel + 1, colVessel);
-                } else {
-                    stilMoving = move(matrix, 0, colVessel);
-                }
-            } else if (direction.equals("right")) {
-                if (isInBound(matrix, rolVessel, colVessel + 1)) {
-                    stilMoving = move(matrix, rolVessel, colVessel + 1);
-                } else {
-                    stilMoving = move(matrix, rolVessel, 0);
-                }
-            } else {
-                if (isInBound(matrix, rolVessel, colVessel - 1)) {
-                    stilMoving = move(matrix, rolVessel, matrix[rolVessel].length);
-                } else {
-                    stilMoving = move(matrix, rolVessel, colVessel);
-                }
+        String command = scanner.nextLine();
+        while (!command.equals("collect the nets") && !fallIntoWhirlpool) {
+            if (command.equals("up")) {
+                move(matrix, rowVessel - 1, colVessel);
+            } else if (command.equals("down")) {
+                move(matrix, rowVessel + 1, colVessel);
+            } else if (command.equals("left")) {
+                move(matrix, rowVessel, colVessel - 1);
+            } else if (command.equals("right")) {
+                move(matrix, rowVessel, colVessel + 1);
             }
-
-            direction = scanner.nextLine();
+            command = scanner.nextLine();
         }
+        if (fallIntoWhirlpool) {
+            System.out.printf("You fell into a whirlpool! The ship sank and you lost the fish you caught. Last coordinates of the ship: [%d,%d]%n", rowVessel, colVessel);
+        }else {
+        if (caughtFish >= 20 ) {
+            System.out.println("Success! You managed to reach the quota!");
+        } else if (caughtFish < 20 && caughtFish >= 0 ) {
+            int diff = 20 - caughtFish;
+            System.out.printf("You didn't catch enough fish and didn't reach the quota! " +
+                    "You need %d tons of fish more.%n", diff);
+        }
+        if (caughtFish > 0) {
+            System.out.printf("Amount of fish caught: %d tons.%n", caughtFish);
+        }
+            printMatrix(matrix);
 
-
-        if (printMatrix) {
-            int diff = fishAmount - 20;
-            if (fishAmount < 20) {
-                System.out.printf("You didn't catch enough fish and didn't reach the quota!" +
-                                " You need %d tons of fish more.%n",
-                        Math.abs(diff));
-            } else {
-                System.out.println("Success! You managed to reach the quota!");
-            }
-            System.out.printf("Amount of fish caught: %d tons.%n", fishAmount);
-            for (char[] chars : matrix) {
-                for (char c : chars) {
-                    System.out.print(c);
-                }
-                System.out.println();
-            }
         }
 
     }
 
-    private static boolean move(char[][] matrix, int nextRow, int nextCol) {
+    private static void move(char[][] matrix, int nextRow, int nextCol) {
+        matrix[rowVessel][colVessel] = '-';
+        if (isOutOfBounds(matrix, nextRow, nextCol)) {
+            if (nextRow < 0 || nextRow >= matrix.length) {
+                nextRow = nextRow < 0 ? matrix.length - 1 : 0;
+            } else {
+                nextCol = nextCol < 0 ? matrix[nextRow].length - 1 : 0;
+            }
+        }
         if (matrix[nextRow][nextCol] == 'W') {
-            rolVessel = nextRow;
+            fallIntoWhirlpool = true;
+            rowVessel = nextRow;
             colVessel = nextCol;
-            System.out.printf(
-                    "You fell into a whirlpool! The ship sank and you lost the fish you caught. Last coordinates of the ship: [%d,%d]%n"
-                    , rolVessel, colVessel);
-            printMatrix = false;
-
-            return false;
-        }
-        char current = matrix[nextRow][nextCol];
-
-        if (Character.isDigit(current)) {
-            fishAmount += Integer.parseInt(String.valueOf(current));
-            matrix[rolVessel][colVessel] = '-';
-            rolVessel = nextRow;
+        } else if (isDigit(matrix[nextRow][nextCol])) {
+            int currentTons = Integer.parseInt(String.valueOf(matrix[nextRow][nextCol]));
+            caughtFish = caughtFish + currentTons;
+            rowVessel = nextRow;
             colVessel = nextCol;
-            matrix[rolVessel][colVessel] = 'S';
-            return true;
+            matrix[rowVessel][colVessel] = 'S';
+        } else {
+            rowVessel = nextRow;
+            colVessel = nextCol;
+            matrix[rowVessel][colVessel] = 'S';
         }
-        matrix[rolVessel][colVessel] = '-';
-        rolVessel = nextRow;
-        colVessel = nextCol;
-        matrix[rolVessel][colVessel] = 'S';
-        return true;
+
+
     }
 
-    private static boolean isInBound(char[][] matrix, int r, int c) {
-        if (r >= 0 && r < matrix.length && c >= 0 && c < matrix[r].length) {
-            return true;
-        } else {
-            return false;
+
+    private static boolean isOutOfBounds(char[][] matrix, int r, int c) {
+        return r < 0 || r >= matrix.length || c < 0 || c >= matrix[r].length;
+    }
+
+    public static void printMatrix(char[][] matrix) {
+        for (char[] chars : matrix) {
+            for (char c : chars) {
+                System.out.print(c);
+            }
+            System.out.println();
         }
-
-
     }
 }
