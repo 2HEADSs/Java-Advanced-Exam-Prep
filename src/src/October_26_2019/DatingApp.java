936e76a7a108ab4573138f254200aa6890987baa
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-02-16 21:46:13 +0200

    Create DatingApp.java

---
+++ src/src/October_26_2019/DatingApp.java
@@ -0,0 +1,73 @@
+package October_26_2019;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class DatingApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String malesInput = scanner.nextLine();
+        String femaleInput = scanner.nextLine();
+        int matches = 0;
+        Deque<Integer> malesStack = new ArrayDeque<>();
+        Deque<Integer> femalesQueue = new ArrayDeque<>();
+
+
+        Arrays.stream(malesInput.split("\\s+"))
+                .map(Integer::parseInt).forEach(malesStack::push);
+
+        Arrays.stream(femaleInput.split("\\s+"))
+                .map(Integer::parseInt).forEach(femalesQueue::offer);
+
+        while (!malesStack.isEmpty() && !femalesQueue.isEmpty()) {
+            if (malesStack.peek() <= 0) {
+                malesStack.pop();
+            } else if (femalesQueue.peek() <= 0) {
+                femalesQueue.poll();
+            } else {
+                //male % 25
+                if (malesStack.peek() % 25 == 0) {
+                    malesStack.pop();
+                    if (!malesStack.isEmpty()) {
+                        malesStack.pop();
+                    }
+                    //female % 25
+                } else if (femalesQueue.peek() % 25 == 0) {
+                    femalesQueue.poll();
+                    if (!femalesQueue.isEmpty()) {
+                        femalesQueue.poll();
+                    }
+                } else {
+                    int male = malesStack.peek();
+                    int female = femalesQueue.peek();
+
+                    if (male == female) {
+                        malesStack.pop();
+                        femalesQueue.poll();
+                        matches++;
+                    } else {
+                        malesStack.pop();
+                        male -= 2;
+                        malesStack.push(male);
+                        femalesQueue.poll();
+                    }
+                }
+            }
+        }
+        System.out.println("Matches: " + matches);
+        if (malesStack.isEmpty()) {
+            System.out.println("Males left: none");
+        } else {
+            System.out.print("Males left: ");
+            System.out.println(malesStack.toString().replace("[", "").replace("]", ""));
+        }
+        if (femalesQueue.isEmpty()) {
+            System.out.println("Females left: none");
+        } else {
+            System.out.print("Females left: ");
+            System.out.println(femalesQueue.toString().replace("[", "").replace("]", ""));
+        }
+    }
+}
