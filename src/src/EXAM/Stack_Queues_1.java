624336c19ae4098ffeb555c7b466b0b4f881744b
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-02-18 22:33:16 +0200

    Update Stack_Queues_1.java

--- src/src/EXAM/Stack_Queues_1.java
+++ src/src/EXAM/Stack_Queues_1.java
@@ -1,10 +1,53 @@
 package EXAM;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
 import java.util.Scanner;
 
 public class Stack_Queues_1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String moneyInput = scanner.nextLine();
+        String priceInput = scanner.nextLine();
+        Deque<Integer> amountMoneyStack = new ArrayDeque<>();
+        Deque<Integer> foodPriceQueue = new ArrayDeque<>();
 
+        int countEatenFood = 0;
+
+        Arrays.stream(moneyInput.split("\\s+"))
+                .map(Integer::parseInt).forEach(amountMoneyStack::push);
+
+        Arrays.stream(priceInput.split("\\s+"))
+                .map(Integer::parseInt).forEach(foodPriceQueue::offer);
+
+        while (!amountMoneyStack.isEmpty() && !foodPriceQueue.isEmpty()) {
+            int currentFoodPrice = foodPriceQueue.poll();
+            int currentMoney = amountMoneyStack.pop();
+
+            if (currentMoney == currentFoodPrice) {
+                countEatenFood++;
+            } else if (currentMoney > currentFoodPrice) {
+                countEatenFood++;
+                int change = currentMoney - currentFoodPrice;
+                if (!amountMoneyStack.isEmpty()) {
+                    int nextMoney = amountMoneyStack.pop();
+                    int newLastMoney = change + nextMoney;
+                    amountMoneyStack.push(newLastMoney);
+                }
+            }
+        }
+        if (countEatenFood >= 4) {
+            System.out.println("Gluttony of the day! Henry ate " + countEatenFood + " foods.");
+        }
+        if (countEatenFood > 1 && countEatenFood < 4) {
+            System.out.println("Henry ate: " + countEatenFood + " foods.");
+        }
+        if(countEatenFood==1){
+            System.out.println("Henry ate: "+countEatenFood+" food.");
+        }
+        if(countEatenFood==0){
+            System.out.println("Henry remained hungry. He will try next weekend again.");
+        }
     }
 }
